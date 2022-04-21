f9de14b1f0d78805c27e0d03b9d36a3654f62112
Marko <markopraakli@gmail.com>
2022-04-21 15:49:47 +0300

    Update code.js

--- GeigerCounter/code.js
+++ GeigerCounter/code.js
@@ -1,31 +1,27 @@
 
-
+console.log('Start');
 
 // WIFI
 var _wifi_ssid = '***',
     _wifi_password = '***';
+    readings = [],
+    CPM = 0;
 
 var wifi = require("Wifi"),
-    http = require("http"),
-    s = new Serial();
+    http = require("http");
 
-var CPM = 0;
-
-s.setup(9600, {
-  rx: NodeMCU.D1,
-  tx: NodeMCU.D2
-});
+// Serial setup
+Serial2.setup(9600, { rx: D25, tx: D26 });
 
 // When a page is requested...
 function pageHandler(req, res)
 {
-  // otherwise write the page out
-  console.log("GET "+req.url);
+  console.log('[HTTP] GET:', req.url);
 
-  if (req.url=="/")
+  if (req.url == '/')
   {
     res.writeHead(200);
-    res.end('<html><head><title>Geiger counter</title></head><body>' + CPM + ' CPM<br />' + (CPM / 122).toFixed(4) + ' uSv/h</body></html>');
+    res.end('<html><head><title>Geiger counter</title></head><body>' + CPM + ' CPM<br />' + (CPM / 122).toFixed(4) + ' uSv/h<hr />' + printReadings() + '</body></html>');
   }
   else
   {
@@ -37,59 +33,93 @@ function pageHandler(req, res)
 // Send data
 function sendData(postURL, data, callback)
 {
-  let content = JSON.stringify(data);
+  // Check if wifi connection exists
+  if( wifi.getStatus().station == 'connected' )
+  {
+    let content = JSON.stringify(data);
 
-  let options = url.parse(postURL, false);
-      options.method = "POST";
-      options.headers = {
-        "Content-Type": "application/json",
-        "Content-Length": content.length
-      };
+    let options = url.parse(postURL, false);
+        options.method = 'POST';
+        options.headers = {
+          'Content-Type': 'application/json',
+          'Content-Length': content.length
+        };
 
-  let req = http.request(options, function(res){
-    let d = "";
-    res.on('data', function(data) { d+= data; });
-    res.on('close', function(data) { callback(d); });
+    let req = http.request(options, res => {
+      let d = '';
+      res.on('data', function(data) { d+= data; });
+      res.on('close', function(data) { callback(d); });
+    });
+
+    req.on('error', e => {
+      callback(e);
+    });
+
+    req.end(content);
+  }
+  else
+  {
+    console.log('[Serial] Unable to send data, WIFI not connected.');
+  }
+}
+
+function printReadings()
+{
+  let str = '';
+  
+  for( let i in readings )
+  {
+    str = i + ' - ' + readings[i] + '<br />';
+  }
+  
+  return str;
+}
+
+function addReadings( value )
+{
+  readings[Math.round(Date.now())] = CPM;
+  readings.splice( 0, readings.length - 10);
+}
+
+function readSerial()
+{
+  Serial2.on('data', data => {
+
+    console.log('[Serial] Data:', data);
+
+    CPM = parseInt(data);
+
+    if( !isNaN(CPM) )
+    {
+      addReadings(CPM);
+      
+      sendData('http://radioaktiivsus.ee/api/cpm', { cpm: CPM }, d => {
+        console.log('[HTTP] Response:', d);
+      });
+    }
   });
-
-  req.on('error', function(e){
-    callback(e);
-  });
-
-  req.end(content);
 }
 
 function onInit()
 {
-  wifi.setHostname('GeigerCounter');
-  wifi.connect( _wifi_ssid, { password: _wifi_password }, () => {
-    console.log('Connected to Wifi. IP address is:', wifi.getIP().ip);
+  console.log('Geiger Counter v0.2');
+
+  // Connect to wifi
+  wifi.connect( _wifi_ssid, { password: _wifi_password }, cb => {
+    wifi.setHostname('GeigerCounter');
+    wifi.setSNTP('1.ee.pool.ntp.org', '2');
   });
 
   // Wifi connected
-  wifi.on('connected', (details) => {
-    console.log('[WiFi]: Connected - YESS.', details);
+  wifi.on('connected', details => {
+    console.log('[WIFI] Connected:', details);
     http.createServer(pageHandler).listen(80);
   });
 
   // Wifi disconnected
-  wifi.on('disconnected', (details) => {
-    console.log('[WiFi]: Disconnected - NOO.', details);
+  wifi.on('disconnected', details => {
+    console.log('[WIFI] Disconnected:', details);
   });
 
-  s.on('data', function (data) {
-
-    let value = parseInt(data);
-
-    if( !isNaN(value) )
-    {
-      CPM = value;
-
-      print('CPM: ' + CPM);
-
-      sendData('http://radioaktiivsus.ee/api/cpm', { cpm: CPM }, function(d) {
-        console.log("HTTP Response: " + d);
-      });
-    }
-  });
+  readSerial();
 }
